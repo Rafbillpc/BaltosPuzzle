491225e4098d7037e2fb9a699713644401a05db5
Rafbill <158373519+Rafbillpc@users.noreply.github.com>
2024-10-06 20:21:07 +0200

    good

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "header.hpp"
+#include <fstream>
 #include <omp.h>
 
 struct puzzle_rot {
@@ -58,28 +59,45 @@ struct puzzle_data {
     }
     
     dist.resize(size);
+    FOR(u, size) dist[u].assign(size, 999'999'999);
     FOR(u, size) {
-      vector<i32> Q(size); i32 nq = 0;
-      vector<i32> D(size, 99999);
-      auto add = [&](i32 v, i32 d) {
-        if(d < D[v]) { D[v] = d; Q[nq++] = v; };
-      };
-      add(u,0);
-      FOR(iq, size) {
-        auto v = Q[iq];
-        FOR(j, 6) add(data[v].data[j], D[v]+1);
+      FOR(x, 6) {
+        i32 v = u;
+        FORU(dx, 0, 2*n) {
+          FOR(y, 6) {
+            i32 w = v;
+            FORU(dy, 0, 2*n) {
+              dist[u][w] = min(dist[u][w], dx*dx+dy*dy);
+              w = data[w].data[y];
+            }
+          }
+          v = data[v].data[x];
+        }
       }
-
-      // FOR(u, size) {
-      //   FOR(j, 6) cerr << data[u].data[j] << ' ';
-      //   cerr << endl;
-      // }
-
-      FOR(v, size) {
-        runtime_assert(D[v] < 99999);
-      }
-      dist[u] = D;
     }
+    
+    // FOR(u, size) {
+    //   vector<i32> Q(size); i32 nq = 0;
+    //   vector<i32> D(size, 99999);
+    //   auto add = [&](i32 v, i32 d) {
+    //     if(d < D[v]) { D[v] = d; Q[nq++] = v; };
+    //   };
+    //   add(u,0);
+    //   FOR(iq, size) {
+    //     auto v = Q[iq];
+    //     FOR(j, 6) add(data[v].data[j], D[v]+1);
+    //   }
+
+    //   // FOR(u, size) {
+    //   //   FOR(j, 6) cerr << data[u].data[j] << ' ';
+    //   //   cerr << endl;
+    //   // }
+
+    //   FOR(v, size) {
+    //     runtime_assert(D[v] < 99999);
+    //   }
+    //   dist[u] = D;
+    // }
 
   }
 };
@@ -348,7 +366,7 @@ struct beam_state {
 };
 
 
-const i32 TREE_SIZE  = 1<<18;
+const i32 TREE_SIZE  = 1<<19;
 const i32 LIMIT_SIZE = TREE_SIZE - 50'000;
 
 using euler_tour_edge = u8;
@@ -428,7 +446,7 @@ void traverse_euler_tour
  i32 istep,
  euler_tour tour_current,
  vector<euler_tour> &tours_next,
- i64* histogram,
+ i64* histogram, i64& low, i64& high,
  i32 cutoff, float cutoff_keep_probability)
 {
   beam_state S; S.reset(P, initial_state, initial_direction);
@@ -459,6 +477,8 @@ void traverse_euler_tour
             auto [v,h] = S.plan_move(P, m);
             auto prev = HS[h&HASH_MASK].exchange(h, std::memory_order_relaxed);
             if(prev != h) {
+              low = min(low, v);
+              high = max(high, v);
               histogram[v] += 1;
               tour_next->push(1+m);
               tour_next->push(0);
@@ -504,7 +524,8 @@ vector<u8> beam_search
   }
 
   beam_state S; S.reset(P, initial_state, initial_direction);
-  i32 max_score = S.total_distance + 16; // TODO
+  i32 max_score = S.total_distance + 1000; // TODO
+  debug(max_score);
   vector<i64> histogram(max_score+1, 0);
   
   vector<euler_tour> tours_current;
@@ -514,17 +535,28 @@ vector<u8> beam_search
   i32   cutoff = max_score;
   float cutoff_keep_probability = 1.0;
 
-  for(i32 istep = 0;; ++istep) {
-    debug(istep);
+  i32 num_threads = omp_get_max_threads();
+  debug(num_threads);
 
+  vector<vector<i64>> L_histograms(num_threads, vector<i64>(max_score+1, 0));
+
+  for(i32 istep = 0;; ++istep) {
     timer timer_s;
     vector<euler_tour> tours_next;
-    histogram.assign(max_score+1,0);
 
+    sort(all(tours_current), [&](auto const& t1, auto const& t2) {
+      return t1.size > t2.size;
+    });
+
+    i64 low = max_score, high = 0;
+    
 #pragma omp parallel
     {
+      vector<i64>& L_histogram = L_histograms[omp_get_thread_num()];
       vector<euler_tour> L_tours_next;
-      vector<i64> L_histogram(max_score+1,0);
+      
+      i64 L_low = max_score, L_high = 0;
+      
       while(1) {
         euler_tour tour_current;
 #pragma omp critical
@@ -542,7 +574,7 @@ vector<u8> beam_search
            istep,
            tour_current, 
            L_tours_next, 
-           L_histogram.data(),
+           L_histogram.data(), L_low, L_high,
            cutoff, cutoff_keep_probability);
 
         #pragma omp critical
@@ -558,7 +590,10 @@ vector<u8> beam_search
           tours_next.eb(L_tours_next.back());
           L_tours_next.pop_back();
         }
-        FOR(i,max_score+1) histogram[i] += L_histogram[i];
+        FORU(i,L_low,L_high) histogram[i] += L_histogram[i];
+        FORU(i,L_low,L_high) L_histogram[i] = 0;
+        low = min(low, L_low);
+        high = max(high, L_high);
       }
     }
 
@@ -570,7 +605,7 @@ vector<u8> beam_search
     { i64 total_count = 0;
       cutoff = max_score;
       cutoff_keep_probability = 1.0;
-      FOR(i, max_score) {
+      FORU(i, low, high) {
         if(total_count+histogram[i] > width) {
           cutoff = i;
           cutoff_keep_probability = (float)(width-total_count) / (float)(histogram[i]);
@@ -578,17 +613,16 @@ vector<u8> beam_search
         }
         total_count += histogram[i];
       }
+      FORU(i, low, high) histogram[i] = 0;
     }
 
     i64 total_size = 0;
     for(auto const& t : tours_next) total_size += t.size;
     
-    i32 low = max_score;
-    FOR(i, max_score+1) if(histogram[i]) { low = i; break; }
-    cerr << setw(3) << istep <<
+    cerr << setw(6) << istep <<
       ": scores = " << setw(3) << low << ".." << setw(3) << cutoff <<
       ", tree size = " << setw(12) << total_size <<
-      ", num trees = " << setw(6) << tours_next.size() <<
+      ", num trees = " << setw(4) << tours_next.size() <<
       ", elapsed = " << setw(10) << timer_s.elapsed() << "s" <<
       endl;
 
@@ -632,24 +666,16 @@ vector<u8> beam_search
   return {};
 }
 
-void solve(puzzle_data const& P, puzzle_state initial_state, i64 width) {
+void solve(puzzle_data const& P,
+           puzzle_state initial_state,
+           u8 initial_directions,
+           i64 width) {
   auto solution = beam_search
-    (P, initial_state, 0, width);
-
-
-  // if(last_direction_src == 0) {
-  //   b = P.data[a].data[move];
-  //   c = P.data[a].data[(move+1)%6];
-  // }else{
-  //   b = P.data[a].data[move];
-  //   c = P.data[a].data[(move+5)%6];
-  // }
-
-
+    (P, initial_state, initial_directions, width);
   
   vector<char> L, R;
-  u8 last_direction_src = 0;
-  u8 last_direction_tgt = 0; 
+  u8 last_direction_src = (initial_directions >> 0 & 1);
+  u8 last_direction_tgt = (initial_directions >> 1 & 1); 
   for(auto m : solution) {
     if(m < 6) {
       if(last_direction_src == 0) {
@@ -674,18 +700,25 @@ void solve(puzzle_data const& P, puzzle_state initial_state, i64 width) {
 
   reverse(all(R));
   L.insert(end(L),all(R));
-  cout << P.n << ":";
+
+  auto cost = L.size();
+  auto filename = "solutions/" + to_string(P.n) + "/" + to_string(cost); 
+  
+  ofstream out(filename);
+  out << P.n << ":";
   for(auto c : L) {
-    cout << c;
+    out << c;
   }
-  cout << endl;
+  out << endl;
+  out.close();
 }
 
 int main(int argc, char** argv) {
-  runtime_assert(argc == 3);
+  runtime_assert(argc == 4);
 
   i64 sz = atoi(argv[1]);
   i64 width = atoll(argv[2]);
+  i32 initial_directions = atoi(argv[3]);
   debug(sz, width);
   
   init_hash();
@@ -696,7 +729,7 @@ int main(int argc, char** argv) {
   runtime_assert(C.count(sz));
   puzzle_data P;
   P.make(sz);
-  solve(P, C[sz], width);
+  solve(P, C[sz], initial_directions, width);
 
   return 0;
 }
